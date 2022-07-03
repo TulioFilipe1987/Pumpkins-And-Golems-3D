66c270ac3262fa2487f81e981b9d5725681e68cd
TulioFilipe1987 <tulio.ruy@gmail.com>
2022-07-03 14:49:34 -0300

    simple game for study

---
+++ Assets/Scripts/Camera Script/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour{
+
+    private Transform pumkinPos;
+    private Pumpkin pumkinScript;
+
+    public float zDistance = 7f;
+    public float yDistance = 4f;
+
+    private string PUMPKING_OBJ = "Pumkin";
+    
+
+    void Awake(){
+        
+        pumkinPos = GameObject.Find(PUMPKING_OBJ).transform;
+        pumkinScript = pumkinPos.gameObject.GetComponent<Pumpkin>();
+        
+
+    }
+
+    
+    void Update(){
+       
+        
+        SetPosition();
+        
+    }
+
+    void SetPosition(){
+        if (pumkinScript.isAlive){
+            Vector3 temp = transform.position;
+            temp.y = pumkinPos.position.y + yDistance;
+            temp.z = pumkinPos.position.z - zDistance;
+            transform.position = temp;
+        }
+    
+    }
+
+
+}// class
---
+++ Assets/Scripts/Pumpkin Scripts/Pumpkin.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour
+{
+    public float moveForce = 10f;
+
+    private Rigidbody myBody;
+
+    [HideInInspector]
+    public bool isAlive;
+
+   
+
+    //private Vector3 initialPosition;
+
+    void Awake(){
+        myBody = GetComponent<Rigidbody>();
+        isAlive = true;
+    }
+
+
+     void Update()
+    {
+        CheckBounds();
+    }
+    
+
+    void FixedUpdate(){
+        MoveThePumkin();
+        
+    }
+
+    void MoveThePumkin(){
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        myBody.AddForce(new Vector3(h * moveForce, 0f, v * moveForce));
+    
+    }
+
+    void CheckBounds(){   
+        if(transform.position.y < -7){
+            Time.timeScale = 0f;
+        }
+        
+    }
+
+
+    void OnTriggerEnter(Collider target){
+        if(target.tag == "Golem"){
+            Time.timeScale = 0f;
+        
+        }
+
+        if(target.tag == "Gates"){
+            Time.timeScale = 0f;
+        }
+
+        
+    }
+
+
+
+
+
+}// clss
